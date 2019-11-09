511cacae20e095bf6b372fcedb8536548605bcb6
Joakim Pettersvold <joakimpokemeg@hotmail.no>
2019-11-09 15:49:03 +0100

    Reworked the attack system with areas. Added test enemy. Replaced root node of player.

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,16 +5,17 @@ const Speed = 350
 var movedir = Vector2(0,0)
 var facing = Vector2(0,1)
 
-var cast_length = 5
-
 var light_attack_power = 5
 var heavy_attack_power = 10
 
+var attack_target
+
 func _ready():
-	$RayCast2D.cast_to = facing * cast_length;
+	copy_area($AttackAreas/Down, $AttackArea)
 
 func _physics_process(delta):
 	controls_loop()
+	facing_update()
 	movement_loop()
 	action_loop()
 
@@ -27,27 +28,54 @@ func controls_loop():
 	movedir.x = -int(Left) + int(Right)
 	movedir.y = -int(Up) + int(Down) 
 	
+#Checks if the player should be faced in another direction based on movement
+func facing_update():
+	if(movedir != Vector2.ZERO && !(movedir.x != 0 && movedir.y != 0) && facing != movedir):
+		facing = movedir;
+		match_attack_area(facing)
+	
 func movement_loop():
 	var motion = movedir.normalized() * Speed
 	move_and_slide(motion, Vector2(0,0))
 	
 func action_loop():
-	var cast = $RayCast2D
-	
-	#If the cast doesn't point in the same direction as we move, change where the cast points
-	if(cast.cast_to.normalized() != movedir && movedir != Vector2.ZERO):
-		cast.cast_to = movedir * cast_length
-		cast.force_raycast_update()
-		
-	#If the cast has found an object and the object can be attacked, attack it
 	var attack_power = 0;
 	if(Input.is_action_just_pressed("light_attack")):
 		attack_power = light_attack_power;
 	elif(Input.is_action_just_pressed("heavy_attack")):
 		attack_power = heavy_attack_power;
 	
-	if(attack_power != 0 && cast.get_collider() != null):
-		var col = cast.get_collider()
-		if(col.has_method("receive_attack")):
-			col.receive_attack(self, attack_power)
+	#If there is something to attack and an attack button has been clicked: ATTACK!
+	if(attack_power != 0 && attack_target != null):
+		attack_target.receive_attack(self, attack_power)
+		
+#Changes the attackzone so that it matches the way the player faces
+func match_attack_area(var facing):
+	attack_target = null
+	match facing:
+		Vector2.UP:
+			copy_area($AttackAreas/Up, $AttackArea)
+		Vector2.RIGHT:
+			copy_area($AttackAreas/Right, $AttackArea)
+		Vector2.DOWN:
+			copy_area($AttackAreas/Down, $AttackArea)
+		Vector2.LEFT:
+			copy_area($AttackAreas/Left, $AttackArea)
+			
+#Copies position, scale and shape of an area to another area
+func copy_area(var source, var destination):
+	destination.position = source.position
+	destination.scale = source.scale
+	destination.find_node("CollisionShape2D").shape = source.find_node("CollisionShape2D").shape
 	
+#Called through a signal when an area has entered the attackzone
+func _area_entered_attackzone(var area):
+	if(area != $Hitbox && area.is_in_group("hitboxes")):
+		var areaParent = area.get_parent()
+		if(areaParent.has_method("receive_attack")):
+			attack_target = areaParent
+		
+#Called through a signal when an area has exited the attackzone
+func _area_exited_attackzone(var area):
+	if(attack_target == area.get_parent()):
+		attack_target = null
